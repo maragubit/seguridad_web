7b833e865cebbb057dcc6806b73b2d7fa8466082
maragubit <maragubit@gmail.com>
2025-01-14 07:47:12 +0100

    google analytics

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -192,6 +192,15 @@
 
    
 </body>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GCTPTC318N"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GCTPTC318N');
+</script>
 <script>
         document.addEventListener('DOMContentLoaded', function() {
             // Verificar si ya se ha aceptado la política de cookies
